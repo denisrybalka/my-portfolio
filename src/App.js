aa076c998ecd865a430eb93c6014cde3a09134f2
Denis Rybalka <denis.rubalka1@gmail.com>
2022-07-22 21:10:17 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { uid } from 'uid';
+import WorkItem from './components/WorkItem';
 
-function App() {
+import "./scss/App.scss";
+
+import jsonData from "./data.json";
+
+const App = () => {
+  const [data, setData] = React.useState(jsonData.data);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1></h1>
+        <img src={require("./assets/cat.webp")} />
+        {/* <p>I'm glad you're interested in me. Below you can see links on how to get me and a couple of my latest works.</p> */}
+
+        <div className="socials">
+          <a className="social" href="https://github.com/denisrybalka" target={"_blank"}>
+            <img src={require("./assets/github.png")} />
+            <span>Take a look at GitHub</span>
+          </a>
+          <a className="social" href="https://t.me/hahahacker" target={"_blank"}>
+            <img src={require("./assets/telegram.png")} />
+            <span>Contact me on Telegram</span>
+          </a>
+          <a className="social" href="mailto:denis.rubalka1@gmail.com" target={"_blank"}>
+            <img src={require("./assets/mail.png")} />
+            <span>Send me an email</span>
+          </a>
+        </div>
+
+          <h2>😎 My besties list:</h2>
+        <article>
+          <h2>❤️ HTML5/CSS ❤️</h2>
+          <h2>❤️ SASS/SCSS ❤️</h2>
+          <h2>❤️ JavaScript ❤️</h2>
+          <h2>❤️ TypeScript ❤️</h2>
+          <h2>❤️ React ❤️</h2>
+          <h2>❤️ Redux ❤️</h2>
+          <h2>❤️ NodeJS ❤️</h2>
+          <h2>❤️ MongoDB ❤️</h2>
+          <h2>❤️ Git ❤️</h2>
+        </article>
+
+        <h2>🧐 My latest works</h2>
+        <div className="itemsList">
+          {
+            data.map((item, el) => {
+              const id = uid();
+              return (
+                <WorkItem
+                  scotchNumber={(el % 4) + 1}
+                  data={{...item, id}}
+                  key={id}
+                />
+              )
+            })
+          }
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/WorkItem.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import "../scss/workItem.scss";
+
+import { ReactComponent as EyeIcon } from "../assets/eye.svg";
+import { ReactComponent as LinkIcon } from "../assets/link.svg";
+
+const WorkItem = ({ scotchNumber, data }) => {
+    const { id, title, livePreviewLink, imageUrl, description, codeReviewLink } = data;
+    return (
+        <div className="workItem">
+            <div className={`img-tape img-tape--${scotchNumber}`}>
+                <img src={require(`../assets/works/${imageUrl}/cover.png`)} />
+                <div className="hidden">
+                    <div className="info">
+                        <p className="title">{title}</p>
+                        <p>{description}</p>
+
+                        <div className="navigation">
+                            <a href={livePreviewLink} target="_blank">
+                                <span className="yellow">
+                                    Live Preview
+                                    <EyeIcon className="icon" />
+                                </span>
+                            </a>
+                            <a href={codeReviewLink} target="_blank">
+                                <span className="red">
+                                    Code Review
+                                    <LinkIcon className="icon" />
+                                </span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default WorkItem
